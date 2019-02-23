5404728f4e2acbb7958e89bd4a5a574ac36ee4e3
rek <rek@Anettes-MacBook-Pro.local>
2019-02-23 10:04:43 +0545

    more initial setup

---
+++ src/links.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+import { 
+    Link,
+  } from "react-router-dom";
+
+export default () => {
+    return (
+        <ul>
+            <li>
+                <Link to="/">Home</Link>
+            </li>
+            <li>
+               <Link to="/01">1 - Controlled</Link>
+            </li>
+            <li>
+                <Link to="/02">2 - State Management</Link>
+            </li>
+            <li>
+               <Link to="/03">3 - Hooks</Link>
+            </li>                                
+            <li>
+                <Link to="/04">4 - Example usage</Link>
+            </li>                                
+        </ul>
+    )
+}
